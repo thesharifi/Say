0e1dfd0f5e0ed08c9072ef825f8536790621cb04
Maruf <thesharifi.maruf@gmail.com>
2023-07-08 22:59:29 +0430

    added dark content status bar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import { RootNavigator } from "./src/navigators/root-navigator";
 
 export default function App() {
-  return <RootNavigator />;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <RootNavigator />
+    </>
+  );
 }
